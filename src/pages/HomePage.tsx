aaed71407658e5558f032117b966e1b408c90484
suttirakcha <suttirak.char@gmail.com>
2025-07-04 17:28:25 +0700

    feat: update the homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,47 +1,82 @@
-import { LearnIcon } from "@/components/icons/LearnIcon";
+import homeHeroImg from "@/assets/home-hero-img.svg";
+import freeAccessImg from "@/assets/free-access-img.svg";
+import noAdsImg from "@/assets/no-ads-img.svg";
+import learnAnywhereImg from "@/assets/learn-anywhere-img.svg";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 
 function HomePage() {
   return (
     <main className="max-lg:text-center">
-      <section className="items-center gap-12 py-20">
-        <div className="grid lg:grid-cols-2 items-center p-8 gap-10 max-w-[1000px] mx-auto">
-          <div className="flex max-lg:justify-center">
-            <LearnIcon className="w-2/5" />
-          </div>
-          <div className="flex flex-col gap-8">
-            <h1 className="title">Let's have fun from learning a language</h1>
+      <section className="flex items-center gap-12 h-[80vh] max-h-[600px] relative">
+        <img
+          src={homeHeroImg}
+          className="absolute top-0 left-0 -z-1 w-full h-full opacity-40 object-cover"
+        />
+        <div className="grid lg:grid-cols-2 items-center p-12 justify-between max-w-[1300px] w-full mx-auto">
+          <div className="flex flex-col gap-12">
+            <h1 className="title-xl">
+              Let's have fun from <br />
+              learning a language
+            </h1>
             <Link to="/register">
-              <Button className="main-btn">Get started</Button>
+              <Button className="main-btn w-[320px]">Get started</Button>
             </Link>
           </div>
         </div>
       </section>
-      <section className="bg-orange-200 py-20">
-        <div className="grid lg:grid-cols-2 items-center p-8 mx-auto max-w-[1000px]">
-          <div className="flex flex-col gap-8">
-            <h1 className="title">Learn from anywhere</h1>
-            <p>
-              Whenever you live at home, office, or at school, you will freely
-              learn anything from this app
-            </p>
-            <button>Get started</button>
+      <section className="py-20">
+        <div className="grid lg:grid-cols-2 items-center p-12 mx-auto max-w-[1300px]">
+          <div className="flex justify-start">
+            <img
+              src={freeAccessImg}
+              className="w-[400px] h-[400px] rounded-full"
+            />
           </div>
-          <div className="">Mascot</div>
+          <div className="flex flex-col gap-8 text-end">
+            <h1 className="title-xl">Completely Free Access</h1>
+            <p>
+              All courses are 100% free, so you can explore and learn without
+              worrying about subscription fees or hidden costs.
+            </p>
+          </div>
         </div>
       </section>
-      <section className="bg-orange-200 py-20">
-        <div className="grid lg:grid-cols-2 items-center p-8 mx-auto max-w-[1000px]">
+      <section className="py-20">
+        <div className="grid lg:grid-cols-2 items-center p-12 mx-auto max-w-[1300px]">
           <div className="flex flex-col gap-8">
-            <h1 className="title">Learn from anywhere</h1>
+            <h1 className="title-xl">Ad-Free Experience</h1>
             <p>
-              Whenever you live at home, office, or at school, you will freely
-              learn anything from this app
+              Enjoy uninterrupted learning with no annoying ads — focus entirely
+              on mastering your new language.
             </p>
-            <button>Get started</button>
           </div>
-          <div className="">Mascot</div>
+          <div className="flex justify-end">
+            <img src={noAdsImg} className="w-[400px] h-[400px] rounded-full" />
+          </div>
+        </div>
+      </section>
+      <section className="pt-20">
+        <div className="grid lg:grid-cols-2 items-center p-12 pb-0 mx-auto max-w-[1300px]">
+          <div className="flex justify-start">
+            <img src={learnAnywhereImg} className="w-[600px] h-[600px]" />
+          </div>
+          <div className="flex flex-col gap-8 text-end">
+            <h1 className="title-xl">Learn Anytime, Anywhere</h1>
+            <p>
+              Learn at your own pace, on any device, wherever you are. Perfect
+              for busy schedules and on-the-go lifestyles.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section className="py-20 bg-orange-200">
+        <div className="flex flex-col gap-8 text-center">
+          <h1 className="title-xl">Learn Anytime, Anywhere</h1>
+          <p>
+            Learn at your own pace, on any device, wherever you are. Perfect for
+            busy schedules and on-the-go lifestyles.
+          </p>
         </div>
       </section>
     </main>
