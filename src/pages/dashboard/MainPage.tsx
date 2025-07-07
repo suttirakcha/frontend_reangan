e48ad53ce107c970b911111de39e64ee3d9f4939
suttirakcha <suttirak.char@gmail.com>
2025-07-07 11:01:24 +0700

    feat: create enrolled courses sidebar

--- src/pages/dashboard/MainPage.tsx
+++ src/pages/dashboard/MainPage.tsx
@@ -1,20 +1,24 @@
-import { useEffect, useState } from "react";
 import { quotes } from "@/lib/quotes";
-import DashboardSection from "@/components/custom/DashboardSection";
+import DashboardSection from "@/components/dashboard/DashboardSection";
+import EnrolledCoursesSidebar from "@/components/custom/EnrolledCoursesSidebar";
 
 function MainPage() {
   const quote = quotes[Math.floor(Math.random() * quotes.length)];
-  // useEffect(() => {}, []);
 
   return (
     <DashboardSection
       title="Welcome to ReanGan"
-      description="What would you like to learn today"
+      description="What would you like to learn today?"
       className="flex flex-col w-full"
     >
-      <div className="border border-orange-500 rounded-md w-full p-6 flex flex-col gap-4">
-        <p className="text-xl font-medium text-center">{quote.text}</p>
-        <p className="text-gray-500 text-end">{quote.author}</p>
+      <div className="grid grid-cols-3 gap-12">
+        <div className="col-span-2">
+          <div className="border border-orange-500 rounded-md w-full p-6 flex flex-col gap-4">
+            <p className="text-xl font-medium text-center">{quote.text}</p>
+            <p className="text-gray-500 text-end">{quote.author}</p>
+          </div>
+        </div>
+        <EnrolledCoursesSidebar />
       </div>
     </DashboardSection>
   );
